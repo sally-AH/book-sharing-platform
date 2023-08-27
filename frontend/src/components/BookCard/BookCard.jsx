ea880b91ffe1a27572d4ec8efa5e6aed7c738635
sally-AH <sallyabdalhafeez@gmail.com>
2023-08-27 15:06:46 +0300

    Frontend <> Added Book Cards

---
+++ frontend/src/components/BookCard/BookCard.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const BookCard = (book) => {
+    console.log(book)
+  return (
+    <>
+      <section className='books'>
+        <div className="container">
+            <div className="box">
+                {/* <img src={book.book.picture} alt="pic" /> */}
+                <img src="https://picsum.photos/200" alt="pic" />
+                <h3>Title: {book.book.name}</h3>
+                <h5>Author: {book.book.author}</h5>
+                <p>Review: {book.book.review}</p>
+            </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default BookCard
--- frontend/src/components/pages/index/Home.jsx
+++ frontend/src/components/pages/index/Home.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import Header from '../../common/Header'
+import { sendRequest } from "../../Core/config/request";
+import BookCard from "../../BookCard/BookCard";
 
 const Home = () => {
-  
+  const [books, setBooks] = useState({list: []});
+
+  const fetchBooks = async () => {
+		try {
+			const response = await sendRequest({
+				method: "GET",
+				route: "/books/",
+			});
+
+
+      setBooks((prevBooks) => ({
+        ...prevBooks,
+        list: response,
+      }));
+      console.log(response)
+		} catch (e) {
+			console.log(e);
+		} 
+	};
+	useEffect(() => {
+		fetchBooks();
+	}, []);
+
+
+
   return (
     <div>
         <Header name1="Home" name2="Add Book" name3="Logout"/>
+        <div className="bookContainer">
+        <div className='content grid3'>
+        {books.list.map((book) => {
+							return <BookCard key={book._id} book={book}/>;
+				})}
+        </div>
+
+        </div>
     </div>
   )
 }
